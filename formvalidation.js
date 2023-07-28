5ec2a72b6aad8d201b76731b511dcbf10e1e628b
Abdul Salam <abdulsalam.ksef18@iba-suk.edu.pk>
2023-07-29 00:10:58 +0500

    updated validation form js file

--- formvalidation.js
+++ formvalidation.js
@@ -1,3 +1,16 @@
-const form = document.getElementById('form');
+const form = document.getElementById('form')
 const email = document.getElementById('email');
-const errorMessage = document.getElementById('error');
+const errorMessage = document.getElementById('errorMsg');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const emailValue=email.value;
+    const lowerCase = emailValue === emailValue.toLowerCase();
+    
+    if(lowerCase){
+        form.submit()
+    }
+    else{
+        errorMessage.textContent="Invalid, Email Must be in lowerCase"
+    }
+});
