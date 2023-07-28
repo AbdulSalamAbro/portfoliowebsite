80e96d1041cf209c0869f0aa26fcffa4ecde99bb
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-07-28 19:13:43 +0600

    Creat dynamic modal

--- index.js
+++ index.js
@@ -123,3 +123,127 @@ for (let i = 0; i < projects.length; i += 1) {
         </div>
       </div> ${fetch}`;
 }
+
+const modalContainer = document.getElementById('popup');
+const popupOn1 = document.getElementById('popupbtn1');
+const popupOn2 = document.getElementById('popupbtn2');
+const popupOn3 = document.getElementById('popupbtn3');
+const popupOn4 = document.getElementById('popupbtn4');
+const popupOn5 = document.getElementById('popupbtn5');
+const popupOn6 = document.getElementById('popupbtn6');
+const popUp = document.getElementById('popup-wrapper');
+const popupOn = [popupOn1, popupOn2, popupOn3, popupOn4, popupOn5, popupOn6];
+
+function openModal() {
+  popUp.style.display = 'block';
+}
+
+document.addEventListener('click', (event) => {
+  function modalBehavior() {
+    const index = popupOn.indexOf(event.target);
+    function closeModal() {
+      popUp.style.display = 'none';
+    }
+
+    function showWebsite(event) {
+      if (event.target === modalContainer) {
+        popUp.style.display = 'none';
+      }
+    }
+
+    for (let i = 0; i < 1; i += 1) {
+      modalContainer.innerHTML = `
+        <div id="modal">
+          <div class="topsection">
+            <div class="offBTN" id="popup-offbtn">&times;</div>
+            <div class="bgPhoto"></div>
+          </div>
+          <div class="bottomsection">
+            <div class="components">
+              <div class="compo-title">
+                  <h3 class="co-title">
+                      ${projects[index].popupTitle}
+                  </h3>
+              </div>
+              <div class="compo-links">
+                <div class="moblink">
+                  <a href="#">${projects[index].technologies[0]}</a>
+                  <a href="#">${projects[index].technologies[1]}</a>
+                  <a href="#">${projects[index].technologies[2]}</a>
+                </div>
+                <div class="desklink">
+                  <a href="#">${projects[index].popupTech[0]}</a>
+                  <a href="#">${projects[index].popupTech[1]}</a>
+                  <a href="#">${projects[index].technologies[2]}</a>
+                  <a href="#">${projects[index].popupTech[2]}</a>
+                  <a href="#">${projects[index].popupTech[3]}</a>
+                  <a href="#">${projects[index].popupTech[4]}</a>
+                </div>
+              </div>
+            </div>
+            <div class="pragraph">
+              <p>${projects[index].popupDes}</p>
+              <p>${projects[index].popupDesTwo}</p>
+            </div>
+            <div class="modalbtn">
+              <a href="#">See live <i class="fa fa-external-link"> </i></a>
+              <a href="#">See source <i class="fa fa-github"></i> </a>
+            </div>
+          </div>
+        </div>`;
+    }
+
+    const popupOff = document.getElementById('popup-offbtn');
+
+    document.addEventListener('click', showWebsite);
+    popupOff.onclick = closeModal;
+    modalContainer.onclick = showWebsite;
+  }
+
+  switch (event.target) {
+    case popupOn[0]:
+    case popupOn[1]:
+    case popupOn[2]:
+    case popupOn[3]:
+    case popupOn[4]:
+    case popupOn[5]:
+      modalBehavior();
+      openModal();
+      break;
+    default:
+  }
+});
+
+const mainForm = document.getElementById('formV');
+const emailInput = document.getElementById('emailV');
+
+const setError = (message) => {
+  const errorDisplay = mainForm.querySelector('.error');
+
+  errorDisplay.innerHTML = message;
+  errorDisplay.style.display = 'block';
+  errorDisplay.style.color = '#c6231b';
+};
+
+const setSuccess = () => {
+  const errorDisplay = mainForm.querySelector('.error');
+
+  errorDisplay.innerHTML = '';
+  errorDisplay.style.display = 'none';
+};
+
+const validateInputs = () => {
+  const emailValue = emailInput.value.trim();
+
+  if (emailValue !== emailValue.toLowerCase()) {
+    setError('The email should be in lower case');
+  } else {
+    setSuccess();
+    mainForm.submit();
+  }
+};
+
+mainForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  validateInputs();
+});
